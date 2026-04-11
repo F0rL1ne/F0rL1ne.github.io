1d9836bafde4ce16563f31f1bb1cace0083dadd7
F0rL1ne <kliompashan10@yandex.com>
2026-04-11 20:00:36 +0700

    save 4

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ function getRandomInt(min, max) {
   let random = getRandomInt(1, 100);
   console.log(random); 
 
-  let input = 
-
+  let input = document.getElementById('myInput')
   input.addEventListener('input', (click) => {
     console.log('Текущий текст:', event.target.value);
   });
 
   function getData() {
-    let numeric = document.getElementById('myInput').value; // Получение значения
+    let numeric = document.getElementById('myInput').value;
+    console.log('Текущий текст:', input.value);
+    if (random > input.value){
+           document.getElementById('myInout').innerHTML = "Итог - больше чем "+input.value
+     
+    }
+    if (random < input.value){
+        document.getElementById('myInout').innerHTML = "Итог - меньше чем "+input.value
+  
+ }
+    if (random = input.value){
+        document.getElementById('myInout').innerHTML = "Итог - победа "+input.value
 
+}
+ // Получение значения
   }
+
+
+
+  const button = document.querySelector('#myButton');
+  button.addEventListener('click', function() {
+    getData();
+  });
+
